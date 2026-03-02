82791118cfb756204586ae0c3954f1c5fcad4ede
Claudio Scheer <claudioscheer@protonmail.com>
2026-03-02 12:34:50 -0300

    feat: add support for new packaging options and update dependencies

--- forge.config.js
+++ forge.config.js
@@ -11,11 +11,31 @@ module.exports = {
   makers: [
     { name: '@electron-forge/maker-zip' },
     {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        format: 'ULFO',
+      },
+    },
+    {
       name: '@electron-forge/maker-deb',
       config: {
         options: {
           icon: appIconPng,
         },
+      },
+    },
+    {
+      name: '@reforged/maker-appimage',
+      config: {
+        options: {
+          icon: appIconPng,
+        },
+      },
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'graphe_bible',
       },
     },
   ],
